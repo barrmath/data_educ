ae951121e0b82f80d11f35bcfa7522ddfe2b7d7a
tux <mathieu.barrios@outlook.com>
2024-04-26 22:59:10 +0200

    modification auto_maj pour avoir un ordre d'execution des notebooks

--- auto_maj.py
+++ auto_maj.py
@@ -6,4 +6,20 @@ os.system('sh datadownload')
 os.chdir('..')
 
 print("execution des notebooks")
-os.system("jupyter execute *.ipynb") 
+
+ordre=[]
+#vérification si un fichier order.txt est présent
+try:
+    with open("order.txt", "r") as f:
+        ordre = f.readlines()
+except:
+    pass
+
+if not ordre:
+    os.system("jupyter execute *.ipynb") 
+ 
+else:
+    for nb in ordre:
+        cmd = "jupyter execute " + nb
+        os.system(cmd)
+ 
